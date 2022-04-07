333b90241d50a19638b56ce67c8a87b89d78c8f2
Ruidy <rnemausat@newstore.com>
2022-04-07 09:11:28 -0400

    deploy 3-word NFT

--- README.md
+++ README.md
@@ -2,7 +2,36 @@
 
 Demo NFT collection for learning purposes.
 
-https://testnets.opensea.io/collection/squarenft-cd2xk3yh7v
+## OPenSea Testnet
+ 1. https://testnets.opensea.io/collection/squarenft-cd2xk3yh7v
+ 2. https://testnets.opensea.io/collection/squarenft-bndes4idpq
+
+## Square NFT
+
+- background color
+  - red
+  - green
+  - blue
+  - black
+- level
+  - epic
+  - legendary
+  - hero
+  - cool
+- class
+  - Lord
+  - Wizzard
+  - Ninja
+  - Rogue
+  - Cleric
+  - Assassin
+- job
+  - BlackSmith
+  - Developer
+  - Merchant
+  - Explorer
+  - Healer
+  - Soldier
 
 ## Built With
 
--- contracts/EpicNFT.sol
+++ contracts/EpicNFT.sol
@@ -16,7 +16,10 @@ contract EpicNFT is ERC721URIStorage {
     function mint() public {
         uint256 tokenID = _tokenIds.current();
         _safeMint(msg.sender, tokenID);
-        _setTokenURI(tokenID, "https://jsonkeeper.com/b/EU51"); // points to the metadata JSON file
+        _setTokenURI(
+            tokenID,
+            "data:application/json;base64,ewogICAgIm5hbWUiOiAiRXBpY0xvcmRIYW1idXJnZXIiLAogICAgImRlc2NyaXB0aW9uIjogIkFuIE5GVCBmcm9tIHRoZSBoaWdobHkgYWNjbGFpbWVkIHNxdWFyZSBjb2xsZWN0aW9uIiwKICAgICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0TkNpQWdJQ0E4YzNSNWJHVStMbUpoYzJVZ2V5Qm1hV3hzT2lCM2FHbDBaVHNnWm05dWRDMW1ZVzFwYkhrNklITmxjbWxtT3lCbWIyNTBMWE5wZW1VNklERTBjSGc3SUgwOEwzTjBlV3hsUGcwS0lDQWdJRHh5WldOMElIZHBaSFJvUFNJeE1EQWxJaUJvWldsbmFIUTlJakV3TUNVaUlHWnBiR3c5SW1Kc1lXTnJJaUF2UGcwS0lDQWdJRHgwWlhoMElIZzlJalV3SlNJZ2VUMGlOVEFsSWlCamJHRnpjejBpWW1GelpTSWdaRzl0YVc1aGJuUXRZbUZ6Wld4cGJtVTlJbTFwWkdSc1pTSWdkR1Y0ZEMxaGJtTm9iM0k5SW0xcFpHUnNaU0krUlhCcFkweHZjbVJJWVcxaWRYSm5aWEk4TDNSbGVIUStEUW84TDNOMlp6ND0iCn0="
+        );
         console.log("NFT %s minted to %s", tokenID, msg.sender);
         _tokenIds.increment();
     }
